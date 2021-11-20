30200d80ae7ce7a331900615f0c36658b961785b
Evan Wies <evan@neomantra.net>
2021-11-19 22:12:40 -0500

    Add --block for block buffering

--- cmd/gotf/main.go
+++ cmd/gotf/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"neomantra/gotf/internal/gotf"
 	"os"
 
@@ -31,14 +32,18 @@ example:
 const DEFAULT_FORMAT = "15:04:05"
 const DEFAULT_FORMAT_WITH_DATE = "2006010-15:04:05"
 
+const BLOCK_BUFFER_SIZE = 4096
+
 func main() {
 	var outputFormat string
 	var useDate bool
+	var blockBuffering bool
 	var globalMatch bool
 	var showHelp bool
 
 	pflag.StringVarP(&outputFormat, "format", "f", "", "golang Time.Format string (default: '15:04:05')")
 	pflag.BoolVarP(&globalMatch, "global", "g", false, "global match")
+	pflag.BoolVarP(&blockBuffering, "block", "b", false, "use block buffering (default: line buffering)")
 	pflag.BoolVarP(&useDate, "date", "d", false, "default format with '2006010-15:04:05'")
 	pflag.BoolVarP(&showHelp, "help", "h", false, "show help")
 	pflag.Parse()
@@ -57,14 +62,36 @@ func main() {
 		}
 	}
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		// convert time
-		str, _ := gotf.ConvertTimes(scanner.Text(), outputFormat, globalMatch)
-		fmt.Println(str)
-	}
+	if blockBuffering {
+		reader := bufio.NewReader(os.Stdin)
+		buf := make([]byte, BLOCK_BUFFER_SIZE)
+		for {
+			n, err := reader.Read(buf)
+			buf = buf[:n]
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				if err != io.ErrUnexpectedEOF {
+					fmt.Fprintln(os.Stderr, err)
+					break
+				}
+			}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+			str, _ := gotf.ConvertTimes(string(buf), outputFormat, globalMatch)
+			fmt.Println(str)
+		}
+	} else {
+		// line buffering with bufio.Scanner
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			// convert time
+			str, _ := gotf.ConvertTimes(scanner.Text(), outputFormat, globalMatch)
+			fmt.Println(str)
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
 	}
 }
