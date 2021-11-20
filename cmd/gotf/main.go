6137980ebc13542f3ee2612ac74495c6a4bc7ae3
Evan Wies <evan@neomantra.net>
2021-11-20 11:12:58 -0500

    Allow passing of filenames on CLI Fix date output format

--- cmd/gotf/main.go
+++ cmd/gotf/main.go
@@ -12,39 +12,85 @@ import (
 
 /////////////////////////////////////////////////////////////////////////////////////
 
-var usageFormat string = `usage:  %s <options>
+var usageFormat string = `usage:  %s <options> [file1 [file2 [...]]]
 
 GOlang Time Formatter
 
-Reads stdin, converts epoch times to human readable, outputs to stdout.
+Reads text files, converting epoch times to human readable, outputting to stdout.
 
 10-digits are interpreted as seconds, 13 as milliseconds,
 16 as microseconds, and 19 as nanoseconds.
 
+If no filenames or only '-' is passed, stdin is processed.
+
 example:
-  cat log.txt | gotf -g | head
+echo 1637421447 | gotf
+
+gotf -g log.txt | head
 
 `
+
+////////////////////////////////////////////////////////////////////////////////////
+// Globals
+
+var g_outputFormat string = ""
+var g_blockBuffering bool = false
+var g_globalMatch bool = false
+
+////////////////////////////////////////////////////////////////////////////////////
+
+func processReader(reader io.Reader) error {
+	if g_blockBuffering {
+		// block buffering with bufio Reader
+		reader := bufio.NewReader(reader)
+		buf := make([]byte, BLOCK_BUFFER_SIZE)
+		for {
+			n, err := reader.Read(buf)
+			buf = buf[:n]
+			if err != nil {
+				if err == io.EOF {
+					return nil
+				}
+				if err != io.ErrUnexpectedEOF {
+					return err
+				}
+			}
+
+			str, _ := gotf.ConvertTimes(string(buf), g_outputFormat, g_globalMatch)
+			fmt.Println(str)
+		}
+	} else {
+		// line buffering with bufio.Scanner
+		scanner := bufio.NewScanner(reader)
+		for scanner.Scan() {
+			// convert time
+			str, _ := gotf.ConvertTimes(scanner.Text(), g_outputFormat, g_globalMatch)
+			fmt.Println(str)
+		}
+
+		if err := scanner.Err(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 /////////////////////////////////////////////////////////////////////////////////////
 // Main Program
 
 const DEFAULT_FORMAT = "15:04:05"
-const DEFAULT_FORMAT_WITH_DATE = "2006010-15:04:05"
+const DEFAULT_FORMAT_WITH_DATE = "2006-01-02 15:04:05"
 
 const BLOCK_BUFFER_SIZE = 4096
 
 func main() {
-	var outputFormat string
 	var useDate bool
-	var blockBuffering bool
-	var globalMatch bool
 	var showHelp bool
 
-	pflag.StringVarP(&outputFormat, "format", "f", "", "golang Time.Format string (default: '15:04:05')")
-	pflag.BoolVarP(&globalMatch, "global", "g", false, "global match")
-	pflag.BoolVarP(&blockBuffering, "block", "b", false, "use block buffering (default: line buffering)")
-	pflag.BoolVarP(&useDate, "date", "d", false, "default format with '2006010-15:04:05'")
+	pflag.StringVarP(&g_outputFormat, "format", "f", "", "golang Time.Format string (default: '15:04:05')")
+	pflag.BoolVarP(&g_globalMatch, "global", "g", false, "global match")
+	pflag.BoolVarP(&g_blockBuffering, "block", "b", false, "use block buffering (default: line buffering)")
+	pflag.BoolVarP(&useDate, "date", "d", false, "default format with '2006-01-02 15:04:05'")
 	pflag.BoolVarP(&showHelp, "help", "h", false, "show help")
 	pflag.Parse()
 
@@ -54,44 +100,33 @@ func main() {
 		os.Exit(0)
 	}
 
-	if len(outputFormat) == 0 {
+	if len(g_outputFormat) == 0 {
 		if useDate {
-			outputFormat = DEFAULT_FORMAT_WITH_DATE
+			g_outputFormat = DEFAULT_FORMAT_WITH_DATE
 		} else {
-			outputFormat = DEFAULT_FORMAT
+			g_outputFormat = DEFAULT_FORMAT
 		}
 	}
 
-	if blockBuffering {
-		reader := bufio.NewReader(os.Stdin)
-		buf := make([]byte, BLOCK_BUFFER_SIZE)
-		for {
-			n, err := reader.Read(buf)
-			buf = buf[:n]
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				if err != io.ErrUnexpectedEOF {
-					fmt.Fprintln(os.Stderr, err)
-					break
-				}
-			}
-
-			str, _ := gotf.ConvertTimes(string(buf), outputFormat, globalMatch)
-			fmt.Println(str)
+	// figure out files
+	filenames := pflag.Args()
+	if len(filenames) == 0 || (len(filenames) == 1 && filenames[0] == "-") {
+		if err := processReader(os.Stdin); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
-	} else {
-		// line buffering with bufio.Scanner
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			// convert time
-			str, _ := gotf.ConvertTimes(scanner.Text(), outputFormat, globalMatch)
-			fmt.Println(str)
+		return
+	}
+	for _, filename := range filenames {
+		f, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
+		defer f.Close()
+		if err := processReader(f); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
 	}
 }
